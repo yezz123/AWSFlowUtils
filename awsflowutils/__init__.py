b7eb9a0a229ec637de949615445d32e55472a74c
Yasser Tahiri <yasserth19@gmail.com>
2023-05-16 03:22:52 +0400

    ✨ Initialize the work on `awsflowutils`

---
+++ awsflowutils/__init__.py
@@ -0,0 +1,10 @@
+"""
+AWSFlowUtils is a user-friendly package for accessing AWS S3 and Redshift.
+
+It streamlines common data tasks, offering simplicity and robustness to enhance your data workflow.
+
+It simplifies data access without handling credential management, making it a reliable solution for seamless data operations.✨
+"""
+
+
+__version__ = "1.0.0"
---
+++ tests/test_version.py
@@ -0,0 +1,5 @@
+import awsflowutils
+
+
+def test_version() -> None:
+    assert awsflowutils.__version__ == "1.0.0"
